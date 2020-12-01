914ffb85dc3ae62a940f708615923f0fe475b1e9
Corinne Dixon <67010820+corinnedixon@users.noreply.github.com>
2020-12-01 09:51:35 -0500

    Sos menu updated

--- screens.py
+++ screens.py
@@ -4,62 +4,73 @@ import tkinter.font as font
 #*************************************HELPER FUNCTIONS**************************************
 
 def sos():
+    # Create window for help menu
+    sosMenu = Toplevel()
+    sosMenu.title("Full Line Help Menu")
+    sosMenu.geometry('800x480')
+    top.overrideredirect(0)
+    sosMenu.overrideredirect(1)
+    
     # Fonts
     answerFont = font.Font(family='Helvetica', size=30, weight='normal')
     questionFont = font.Font(family='Helvetica', size=20, weight='normal')
     
     # Questions
-    q1 = Text(top, font=questionFont, height=1, width=10)
+    q1 = Text(sosMenu, font=questionFont, height=1, width=10)
     q1.insert(INSERT, "Is it saucing the 14 Inch Pizza?")
     q1.place(x=25, y=20)
     
-    q2 = Text(top, font=questionFont, height=1, width=10)
+    q2 = Text(sosMenu, font=questionFont, height=1, width=10)
     q2.insert(INSERT, "Is it saucing the 12 Inch Pizza?")
     q2.place(x=25, y=60)
     
-    q3 = Text(top, font=questionFont, height=1, width=10)
+    q3 = Text(sosMenu, font=questionFont, height=1, width=10)
     q3.insert(INSERT, "Is it saucing the 10 Inch Pizza?")
     q3.place(x=25, y=100)
     
-    q4 = Text(top, font=questionFont, height=1, width=10)
+    q4 = Text(sosMenu, font=questionFont, height=1, width=10)
     q4.insert(INSERT, "Is it saucing the 7 Inch Pizza?")
     q4.place(x=25, y=140)
     
-    q5 = Text(top, font=questionFont, height=1, width=10)
+    q5 = Text(sosMenu, font=questionFont, height=1, width=10)
     q5.insert(INSERT, "Do intake tubes have air bubbles?")
     q5.place(x=25, y=180)
     
-    q6 = Text(top, font=questionFont, height=1, width=10)
+    q6 = Text(sosMenu, font=questionFont, height=1, width=10)
     q6.insert(INSERT, "Is the turntable motor shaft spinning?")
     q6.place(x=25, y=220)
     
-    q7 = Text(top, font=questionFont, height=1, width=10)
+    q7 = Text(sosMenu, font=questionFont, height=1, width=10)
     q7.insert(INSERT, "Is it saucing the 14 Inch Pizza?")
     q7.place(x=25, y=260)
     
-    q5 = Text(top, font=questionFont, height=1, width=10)
+    q5 = Text(sosMenu, font=questionFont, height=1, width=10)
     q5.insert(INSERT, "Do intake tubes have air bubbles?")
     q5.place(x=25, y=300)
     
-    q6 = Text(top, font=questionFont, height=1, width=10)
+    q6 = Text(sosMenu, font=questionFont, height=1, width=10)
     q6.insert(INSERT, "Is the turntable motor shaft spinning?")
     q6.place(x=25, y=340)
     
-    q7 = Text(top, font=questionFont, height=1, width=10)
+    q7 = Text(sosMenu, font=questionFont, height=1, width=10)
     q7.insert(INSERT, "Is the screen functioning properly?")
     q7.place(x=25, y=380)
     
-    q8 = Text(top, font=questionFont, height=1, width=10)
+    q8 = Text(sosMenu, font=questionFont, height=1, width=10)
     q8.insert(INSERT, "Can you hear any grinding noise?")
     q8.place(x=25, y=420)
     
-    q9 = Text(top, font=questionFont, height=1, width=10)
+    q9 = Text(sosMenu, font=questionFont, height=1, width=10)
     q9.insert(INSERT, "Can you hear any high pitched noise?")
     q9.place(x=25, y=460)
     
-    q10 = Text(top, font=questionFont, height=1, width=10)
+    q10 = Text(sosMenu, font=questionFont, height=1, width=10)
     q10.insert(INSERT, "Did this problem just start?")
     q10.place(x=25, y=500)
+    
+    # Back button
+    back  = Button(sosMenu, text = "Back", font = subFont, fg="black", bg = "white", command = sosMenu.destroy, height = 2, width = 6)
+    back.place(x=550, y=350)
 
     print("SOS\n")
     
