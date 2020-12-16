e3ba0dda8c551ea08d8ed00684e42bc2eb02c56a
Corinne Dixon <67010820+corinnedixon@users.noreply.github.com>
2020-12-16 12:00:14 -0500

    encode to ascii

--- testboard.py
+++ testboard.py
@@ -15,33 +15,33 @@ s4_speed = 50 # Sauce stepper motor 4 speed
 
 line1 = "$STEPPER_START,PUMP1,FORWARD," + str(s1_speed) + ",0\n"
 
-ser.write(line1.encode())
+ser.write(line1.encode("ascii"))
 line2 = "$STEPPER_START,PUMP2,FORWARD," + str(s2_speed) + ",0\n"
-ser.write(line2.encode())
+ser.write(line2.encode("ascii"))
 line3 = "$STEPPER_START,PUMP3,FORWARD," + str(s3_speed) + ",0\n"
 
-ser.write(line3.encode())
+ser.write(line3.encode("ascii"))
 line4 ="$STEPPER_START,PUMP4,FORWARD," + str(s4_speed) + ",0\n"
 
-ser.write(line4.encode())
+ser.write(line4.encode("ascii"))
 
 time.sleep(2)
 ser.reset_output_buffer()
 
 stop1 = "$STEPPER_STOP,PUMP1\n"
 
-ser.write(stop1.encode())
+ser.write(stop1.encode("ascii"))
 stop2 = "$STEPPER_STOP,PUMP2\n"
 
-ser.write(stop2.encode())
+ser.write(stop2.encode("ascii"))
 stop3 = "$STEPPER_STOP,PUMP3\n"
 
 ser.write(stop3.encode())
 stop4 = "$STEPPER_STOP,PUMP4\n"
 
-ser.write(stop4.encode())
+ser.write(stop4.encode("ascii"))
 
 # Spin turn table for 1000 steps at 1 ms per step
 spin = "$STEPPER_START,TURNTABLE,FORWARD,1000,1\n"
 
-ser.write(spin.encode())
+ser.write(spin.encode("ascii"))
