b439743704168c089f725beb42b8208f177fa95d
Corinne Dixon <67010820+corinnedixon@users.noreply.github.com>
2020-12-16 12:17:24 -0500

    another attempt

--- testboard.py
+++ testboard.py
@@ -12,35 +12,34 @@ s4_speed = 50 # Sauce stepper motor 4 speed
 
 # Start sauce pumps for 2 seconds
 
-line1 = "$STEPPER_START,PUMP1,FORWARD," + str(s1_speed) + ",0
-"
+line1 = "$STEPPER_START,PUMP1,FORWARD," + str(s1_speed) + ",0\r\n"
 
-ser.write(line1.encode("ascii"))
-line2 = "$STEPPER_START,PUMP2,FORWARD," + str(s2_speed) + ",0\n"
-ser.write(line2.encode("ascii"))
-line3 = "$STEPPER_START,PUMP3,FORWARD," + str(s3_speed) + ",0\n"
+ser.write(line1.encode())
+line2 = "$STEPPER_START,PUMP2,FORWARD," + str(s2_speed) + ",0\r\n"
+ser.write(line2.encode())
+line3 = "$STEPPER_START,PUMP3,FORWARD," + str(s3_speed) + ",0\r\n"
 
-ser.write(line3.encode("ascii"))
-line4 ="$STEPPER_START,PUMP4,FORWARD," + str(s4_speed) + ",0\n"
+ser.write(line3.encode())
+line4 ="$STEPPER_START,PUMP4,FORWARD," + str(s4_speed) + ",0\r\n"
 
-ser.write(line4.encode("ascii"))
+ser.write(line4.encode())
 
 time.sleep(2)
 
-stop1 = "$STEPPER_STOP,PUMP1\n"
+stop1 = "$STEPPER_STOP,PUMP1\r\n"
 
-ser.write(stop1.encode("ascii"))
-stop2 = "$STEPPER_STOP,PUMP2\n"
+ser.write(stop1.encode())
+stop2 = "$STEPPER_STOP,PUMP2\r\n"
 
-ser.write(stop2.encode("ascii"))
-stop3 = "$STEPPER_STOP,PUMP3\n"
+ser.write(stop2.encode())
+stop3 = "$STEPPER_STOP,PUMP3\r\n"
 
 ser.write(stop3.encode())
-stop4 = "$STEPPER_STOP,PUMP4\n"
+stop4 = "$STEPPER_STOP,PUMP4\r\n"
 
-ser.write(stop4.encode("ascii"))
+ser.write(stop4.encode())
 
 # Spin turn table for 1000 steps at 1 ms per step
-spin = "$STEPPER_START,TURNTABLE,FORWARD,1000,1\n"
+spin = "$STEPPER_START,TURNTABLE,FORWARD,1000,1\r\n"
 
-ser.write(spin.encode("ascii"))
+ser.write(spin.encode())
