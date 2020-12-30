0f16b4aabf320d49f02681ad9ac8c287486ef52d
Corinne Dixon <67010820+corinnedixon@users.noreply.github.com>
2020-12-30 13:58:50 -0500

    Should be functioning properly

--- newboard.py
+++ newboard.py
@@ -15,7 +15,7 @@ import urllib.request
 #*************************************START CONNECTION**************************************
 
 # Open UART serial connection
-###ser = serial.Serial("/dev/ttyS0", 115200)  # opens port with baud rate
+ser = serial.Serial("/dev/ttyS0", 115200)  # opens port with baud rate
 
 #**************************************FIREBASE SET UP**************************************
 
@@ -172,12 +172,12 @@ def sauceProgram(button):
 #Functions for starting and stopping spin
 def spinFunc():
   spin = "$STEPPER_START,TURNTABLE,FORWARD,30000,0\r\n"
-  ###ser.write(spin.encode())
+  ser.write(spin.encode())
   print(spin)
 
 def stopSpinning():
   stop = "$STEPPER_STOP,TURNTABLE\r\n"
-  ###ser.write(stop.encode())
+  ser.write(stop.encode())
   print(stop)
 
 #Functions for starting and stopping sauce
@@ -185,35 +185,35 @@ def pumpProgram(size):
     # Start pumping infinitely based on size
     start1 = "$STEPPER_START,PUMP1,FORWARD," + str(s1_speed) + ",0\r\n"
     print(start1)
-    ###ser.write(start1.encode())
+    ser.write(start1.encode())
     if size >= 10:
         start2 = "$STEPPER_START,PUMP2,FORWARD," + str(s2_speed) + ",0\r\n"
         print(start2)
-        ###ser.write(start2.encode())
+        ser.write(start2.encode())
     if size >= 12:
         start3 = "$STEPPER_START,PUMP3,FORWARD," + str(s3_speed) + ",0\r\n"
         print(start3)
-        ###ser.write(start3.encode())
+        ser.write(start3.encode())
     if size >= 14:
         start4 = "$STEPPER_START,PUMP4,FORWARD," + str(s4_speed) + ",0\r\n"
         print(start4)
-        ###ser.write(start4.encode())
+        ser.write(start4.encode())
 
 def stopPumping():
   global pumping
   pumping = False
   stop1 = "$STEPPER_STOP,PUMP1\r\n"
   print(stop1)
-  ###ser.write(stop1.encode())
+  ser.write(stop1.encode())
   stop2 = "$STEPPER_STOP,PUMP2\r\n"
   print(stop2)
-  ###ser.write(stop2.encode())
+  ser.write(stop2.encode())
   stop3 = "$STEPPER_STOP,PUMP3\r\n"
   print(stop3)
-  ###ser.write(stop3.encode())
+  ser.write(stop3.encode())
   stop4 = "$STEPPER_STOP,PUMP4\r\n"
   print(stop4)
-  ###ser.write(stop4.encode())
+  ser.write(stop4.encode())
 
 #**************************************CLEAN AND PRIME**************************************
 
@@ -243,16 +243,16 @@ def cleanProgram(button):
 
     # Pump for 2 minutes
     start1 = "$STEPPER_START,PUMP1,FORWARD," + str(clean_prime_speed) + ",0\r\n"
-    ###ser.write(start1.encode())
+    ser.write(start1.encode())
     start2 = "$STEPPER_START,PUMP2,FORWARD," + str(clean_prime_speed) + ",0\r\n"
-    ###ser.write(start2.encode())
+    ser.write(start2.encode())
     start3 = "$STEPPER_START,PUMP3,FORWARD," + str(clean_prime_speed) + ",0\r\n"
-    ###ser.write(start3.encode())
+    ser.write(start3.encode())
     start4 = "$STEPPER_START,PUMP4,FORWARD," + str(clean_prime_speed) + ",0\r\n"
-    ###ser.write(start4.encode())
+    ser.write(start4.encode())
     while((not shutdown) and (time.time()-cleanTime < 120)):
         pass
-    ###stopPumping()
+    stopPumping()
     
     # Update running - cleaning is done
     running = False
@@ -284,16 +284,16 @@ def primeProgram(button):
 
     # Pump for 30 seconds
     start1 = "$STEPPER_START,PUMP1,FORWARD," + str(clean_prime_speed) + ",0\r\n"
-    ###ser.write(start1.encode())
+    ser.write(start1.encode())
     start2 = "$STEPPER_START,PUMP2,FORWARD," + str(clean_prime_speed) + ",0\r\n"
-    ###ser.write(start2.encode())
+    ser.write(start2.encode())
     start3 = "$STEPPER_START,PUMP3,FORWARD," + str(clean_prime_speed) + ",0\r\n"
-    ###ser.write(start3.encode())
+    ser.write(start3.encode())
     start4 = "$STEPPER_START,PUMP4,FORWARD," + str(clean_prime_speed) + ",0\r\n"
-    ###ser.write(start4.encode())
+    ser.write(start4.encode())
     while((not shutdown) and (time.time()-primeTime < 30)):
         pass
-    ###stopPumping()
+    stopPumping()
     
     # Update running - priming is done
     running = False
