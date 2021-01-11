fbbaef9876a5594e58945885e62d5e24361f44e5
Corinne Dixon <67010820+corinnedixon@users.noreply.github.com>
2021-01-11 17:35:00 -0500

    submit move 5 demo

--- testboard.py
+++ testboard.py
@@ -507,7 +507,7 @@ def sos():
     
     # Back button
     done  = Button(sosMenu, text = "SUBMIT FORM", font = otherFont, bg = button_color, fg = main_fg, command = lambda: send(answers, sosMenu), height = 2, width = 12)
-    done.place(x=545, y=20)
+    done.place(x=540, y=50)
     back  = Button(sosMenu, text = "BACK", font = otherFont, bg = button_color, fg = main_fg, command = sosMenu.destroy, height = 2, width = 6)
     back.place(x=650, y=380)
 
